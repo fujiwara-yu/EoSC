c30a53369eee63df5efb7a3efe5792c24ef39c1a
Yuuki Fujiwara <fujiwara2018@s.okayama-u.ac.jp>
2020-09-28 16:09:21 +0900

    Refactoring

--- collect.py
+++ collect.py
@@ -10,121 +10,135 @@ import lib.database
 def rate_limit_controll():
     # APIの制限メッセージ
     rate_remaining = client.rate_limiting
-    print("limit: %d" % rate_remaining)
-    if (rate_remaining < 10):
+    print(f"limit: {rate_remaining}")
+    if (rate_remaining[0] < 10):
         print("API limit\nPlease wait 1 hour")
         time.sleep(3600)
 
 # PRをDBに保存する
-def get_pull_list():
-    print("Get pull start")
-    insert_pulls = "INSERT IGNORE INTO pulls (id, number, username, body, created_at, closed_at, merged_at, state, commits, additions, deletions, changed_files, branch) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+def get_pull_requests():
+    print("Get pull_requests start")
+    insert_pull_requests = "INSERT IGNORE INTO pull_requests (id, github_number, name, creator_name, body, created_at, closed_at, merged_at, state, commits, additions, deletions, changed_files, branch) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"
+
     for pr in pulls:
-        p = ()
-        p = (pr.id, pr.number, pr.user.login, pr.body, pr.created_at, pr.closed_at, pr.merged_at, pr.state, pr.commits, pr.additions, pr.deletions, pr.changed_files, pr.base.repo.default_branch)
-        print(pr.id, pr.number, pr.user.login, pr.body, pr.created_at, pr.closed_at, pr.merged_at, pr.state, pr.commits, pr.additions, pr.deletions, pr.changed_files, pr.base.repo.default_branch)
-        db.insert(insert_pulls, p)
+        insert_data = ()
+        insert_data = (pr.id, pr.number, pr.title, pr.user.login, pr.body, pr.created_at, pr.closed_at, pr.merged_at, pr.state, pr.commits, pr.additions, pr.deletions, pr.changed_files, pr.base.repo.default_branch)
+        print(insert_data)
+        db.insert(insert_pull_requests, insert_data)
         rate_limit_controll()
 
     print("Get pull finish")
+
 # PRとかのcommentをDBに保存
-def get_pull_comment_list():
-    print("Get pull_comment start")
-    insert_comments = "INSERT IGNORE INTO pullreq_comments (id, pr_id, username, created_at, comment) VALUES (%s, %s, %s, %s, %s);"
+def get_pull_request_comments():
+    print("Get pull_request_comments start")
+    insert_comments = "INSERT IGNORE INTO pull_request_comments (id, pull_request_id, creator_name, created_at, body) VALUES (%s, %s, %s, %s, %s);"
+    issues = target_repo.get_issues(state='closed')
     for iss in issues:
         comments = iss.get_comments()
         for prc in comments:
-            x = ()
-            x = (prc.id, iss.number,prc.user.login, prc.created_at, prc.body)
-            print(prc.id, iss.number,prc.user.login, prc.created_at, prc.body)
-            db.insert(insert_comments, x)
+            insert_data = ()
+            insert_data = (prc.id, iss.number, prc.user.login, prc.created_at, prc.body)
+            print(insert_data)
+            db.insert(insert_comments, insert_data)
             rate_limit_controll()
 
     print("Get pull_comment finish")
 
-def get_project_commit_list():
-    print("Get project_commit start")
-    insert_commits = "INSERT IGNORE INTO project_commits (sha, username, filename, created_at, comment, additions, deletions, changes) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);"
-    commits = repo.get_commits()
-    for com in commits:
-        for file in com.files:
-            if com.author is not None:
-                print(com.sha, com.author.login, file.filename, com.commit.author.date, com.commit.message, file.additions, file.deletions, file.changes)
-                x = ()
-                x = (com.sha, com.author.login, file.filename, com.commit.author.date, com.commit.message, file.additions, file.deletions, file.changes)
-                db.insert(insert_commits, x)
-                rate_limit_controll()
-
-    print("Get project_commit finish")
-
-#def commit():
-#    pulls = repo.get_pulls(state='close', sort='created')
-#    for pull in pulls:
-#        commits = pull.get_commits()
-#        for commit in commits:
-#            if pull.state == ""
-#            print(commit.sha)
-#            sql = "UPDATE commits SET pr_id = %s WHERE sha = %s;"
-#            db.insert(sql, (pull.id, commit.sha))
-#            rate_limit_controll()
-
-def get_pull_commit_list():
-    print("Get pull_commit start")
-    insert_commits = "INSERT IGNORE INTO pull_commits (sha, pr_id, username, filename, created_at, comment) VALUES (%s, %s, %s, %s, %s, %s);"
-    pulls = repo.get_pulls(state='close', sort='created')
+def get_pull_commits():
+    print("Get pull_commits start")
+    insert_commits = "INSERT IGNORE INTO commits (sha, pull_request_id, creator_name, created_at, body, state) VALUES (%s, %s, %s, %s, %s, %s);"
+    #pulls = repo.get_pulls(state='close', sort='created')
+    state = "pull_request"
     for pull in pulls:
         commits = pull.get_commits()
         for commit in commits:
-            com = repo.get_commit(commit.sha)
+            com = target_repo.get_commit(commit.sha)
             if com.author is not None:
+                insert_data = ()
+                insert_data = (com.sha, pull.id, com.author.login, com.commit.author.date, com.commit.message, state)
+                print(insert_data)
+                db.insert(insert_commits, insert_data)
+                rate_limit_controll()
+
                 for file in com.files:
-                    print(com.sha, pull.id, com.author.login, file.filename, com.commit.author.date, com.commit.message)
-                    x = ()
-                    x = (com.sha, pull.id, com.author.login, file.filename, com.commit.author.date, com.commit.message)
-                    db.insert(insert_commits, x)
-                    rate_limit_controll()
+                    file_id = add_file(file.filename)
+                    get_commits_files(com.sha, file_id)
 
     print("Get pull_commit finish")
 
-def get_pull_title():
-    print("Get title start")
-    insert_pulls = "INSERT IGNORE INTO title (id, number, title, url) VALUES (%s, %s, %s, %s);"
-    for pr in pulls:
-        p = ()
-        p = (pr.id, pr.number, pr.title, pr.html_url)
-        print(pr.id, pr.number, pr.title, pr.html_url)
-        db.insert(insert_pulls, p)
-        rate_limit_controll()
+def get_project_commits():
+    print("Get project_commits start")
+    insert_commits = "INSERT IGNORE INTO commits (sha, pull_request_id, creator_name, created_at, body, state) VALUES (%s, %s, %s, %s, %s, %s);"
+    commits = target_repo.get_commits()
+    state = "project"
+    for com in commits:
+        if com.author is not None:
+            insert_data = ()
+            insert_data = (com.sha, None, com.author.login, com.commit.author.date, com.commit.message, state)
+            print(insert_data)
 
-    print("Get title finish")
+            query = "UPDATE commits SET state=%s WHERE sha=%s"
+            db.insert(query, ('pull_request_and_project', com.sha))
+            db.insert(insert_commits, insert_data)
+            rate_limit_controll()
+
+            for file in com.files:
+                file_id = add_file(file.filename)
+                get_commits_files(com.sha, file_id)
+
+    print("Get project_commit finish")
+
+
+def get_commits_files(commit_sha, file_id):
+    insert_commits_files = "INSERT IGNORE INTO commits_files (commit_sha, file_id) VALUES (%s, %s);"
+    x = ()
+    x = (commit_sha, file_id)
+    print(x)
+    db.insert(insert_commits_files, x)
+
+def add_file(file):
+    insert_files = "INSERT IGNORE INTO project_files (name) VALUES (%s);"
+    x = ()
+    x = (file,)
+    print(x)
+    db.insert(insert_files, x)
+
+    query = "SELECT id from project_files where name = %s"
+    print(db.select(query, (file,)))
+    return db.select(query, (file,))[0][0]
 
 def run():
-    get_pull_list()
-    get_pull_comment_list()
-    get_project_commit_list()
-    #commit()
-    get_pull_commit_list()
-    get_pull_title()
+    get_pull_requests()
+    get_pull_request_comments()
+    get_pull_commits()
+    get_project_commits()
+
     print("ALL finish")
 
 if __name__ == "__main__":
-    args = sys.argv
+    ## FIX: 引数をyamlから読むようにかえる
 
-    # 引数ないとき
-    if(len(args) != 2):
-        print("Not argument")
-        sys.exit()
+    # yamlファイルの読み込み
+    f = open("settings.yaml", "r")
+    settings = yaml.load(f, Loader=yaml.SafeLoader)
 
     # DB接続
     db = lib.database.Database()
 
     # tokenの設定
-    f = open("settings.yaml", "r")
-    settings = yaml.load(f)
     token = settings["token"]
 
+    # データを収集するリポジトリの設定
+    user = settings["user"]
+    repo = settings["repo"]
+    user_repo = user + "/" + repo
+
     client = Github(token, per_page=100)
-    repo = client.get_repo(args[1])
-    pulls = repo.get_pulls(state='close', sort='created')
-    issues = repo.get_issues(state='closed')
+    target_repo = client.get_repo(user_repo)
+    
+    # pullsをidから1つだけ指定できれば，closedになったPRを保存できる
+    # issuesもかな
+    pulls = target_repo.get_pulls(state='closed', sort='created')
+
     run()
